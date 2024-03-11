8b59228f68af81a631413d6284ce80c81bd356dc
parteekmalik <parteekmalik5@gmail.com>
2024-03-11 14:25:21 +0530

    first run

--- src/server.ts
+++ src/server.ts
@@ -9,26 +9,26 @@ type orderType = {
     bids: { price: number }[];
 };
 const orders: Record<string, orderType> = { BTCUSDT: { asks: [{ price: 70000 }], bids: [{ price: 70600 }] } };
-const ws = new WSbinance("wss://stream.binance.com:9443/ws", {
-    onmessage(data) {
-        const { asks, bids } = orders[data.s];
-        // console.log(data, SymbolOrders);
-        if (asks.length && asks[0].price >= Number(data.p)) {
-            console.log("asks matched");
-            while (asks.length && asks[0].price >= Number(data.p)) {
-                console.log("order complete");
-                asks.shift();
-                
-            }
-        } else if (bids.length && bids[0].price <= Number(data.p)) {
-            console.log("bids matched");
-            while (bids.length && bids[0].price <= Number(data.p)) {
-                console.log("order complete");
-                bids.shift();
-            }
-        }
-    },
-});
+// const ws = new WSbinance("wss://stream.binance.com:9443/ws", {
+//     onmessage(data) {
+//         const { asks, bids } = orders[data.s];
+//         // console.log(data, SymbolOrders);
+//         if (asks.length && asks[0].price >= Number(data.p)) {
+//             console.log("asks matched");
+//             while (asks.length && asks[0].price >= Number(data.p)) {
+//                 console.log("order complete");
+//                 asks.shift();
+
+//             }
+//         } else if (bids.length && bids[0].price <= Number(data.p)) {
+//             console.log("bids matched");
+//             while (bids.length && bids[0].price <= Number(data.p)) {
+//                 console.log("order complete");
+//                 bids.shift();
+//             }
+//         }
+//     },
+// });
 
 const app = express();
 const port = 3000;
